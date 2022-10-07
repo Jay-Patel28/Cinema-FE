abff8e2ddc185ed8b95136cfcb147431db25e591
Jay-Patel28 <jay.patel@ajmerainfotech.com>
2022-10-07 15:42:23 +0530

    temp

--- cypress/e2e/first.cy.ts
+++ cypress/e2e/first.cy.ts
@@ -1,12 +1,16 @@
-describe('My First Test', () => {
-    it('should click on Cinema Heading ', () => {
-        cy.visit('/');
-        cy.contains('Cinema').click()
-        cy.url().should('include', '/')
-    })
-    it('should click on SignIn button ', () => {
-        cy.visit('/');
-        cy.contains('SignIn').click()
-        cy.contains('Login')
-    })
-  })
+describe("My First Test", () => {
+  beforeEach(() => {
+    cy.log("Run !");
+  });
+
+  it("should click on Cinema Heading ", () => {
+    cy.visit("/");
+    cy.contains("Cinema").click();
+    cy.url().should("include", "/");
+  });
+  it("should click on SignIn button ", () => {
+    cy.visit("/");
+    cy.contains("SignIn").click();
+    cy.contains("Login");
+  });
+});
