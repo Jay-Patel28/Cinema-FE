6a66c9c1fd8388217820cc876514149b10899bac
Jay-Patel28 <jay.patel@ajmerainfotech.com>
2022-10-10 18:56:39 +0530

    Added Feature files and tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,8 +3,13 @@ import { defineConfig } from "cypress";
 export default defineConfig({
   projectId: 'ekpiam',
   e2e: {
-    baseUrl: 'http://localhost:3000',
+    baseUrl: "http://localhost:3000",
+
+
     setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.ts")(on, config);
     },
+    excludeSpecPattern: "*.ts",
+    specPattern: "**/*.feature",
   },
 });
