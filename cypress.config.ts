6d44745e7576d8e6395c6a894b8ef688d2bcb0c7
Jay-Patel28 <jay.patel@ajmerainfotech.com>
2022-12-01 16:20:47 +0530

    ds

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,15 +2,16 @@ import { defineConfig } from "cypress";
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 export default defineConfig({
-  projectId: 'ksrcar',
+  projectId: "ksrcar",
   e2e: {
+    video: false,
     baseUrl: "http://localhost:3000",
     experimentalStudio: true,
     setupNodeEvents(on) {
       const options = {
         typescript: require.resolve("typescript"),
       };
-      
+
       on("file:preprocessor", cucumber(options));
     },
     specPattern: "**/*.feature",
