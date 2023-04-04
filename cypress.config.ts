ab6c8356a42b1e580558c294bd3ef2f6e0e0a693
hardikd-ajmerainfotech <hardik.dadukiya@ajmerainfotech.com>
2023-04-04 16:09:26 +0530

    added report34

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 const cucumber = require("cypress-cucumber-preprocessor").default;
 
 export default defineConfig({
-  projectId: "kkfm4d",
+  projectId: 'kkfm4d',
   e2e: {
     baseUrl: "http://localhost:3000",
     experimentalStudio: true,
@@ -10,28 +10,27 @@ export default defineConfig({
       const options = {
         typescript: require.resolve("typescript"),
       };
-
+      
       on("file:preprocessor", cucumber(options));
     },
     specPattern: "**/*.feature",
   },
-  reporter: "cypress-multi-reporters",
+reporter: "cypress-multi-reporters",
   reporterOptions: {
     reporterEnabled: "mochawesome, junit",
     mochawesomeReporterOptions: {
       reportDir: "cypress/reports/mochawesome",
       reportFilename: "report",
       quiet: true,
-      overwrite: true,
+      overwrite: false,
       html: true,
-      json: true,
-      css: true,
+      json: true
     },
     junitReporterOptions: {
       mochaFile: "cypress/reports/junit/test-results-[hash].xml",
-      toConsole: true,
-    },
-  },
+      toConsole: true
+    }
+  }
   // m{
   //   projectId: "kkfm4d",
   //   // The rest of the Cypress config options go here...
